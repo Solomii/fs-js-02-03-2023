5173ededab9077510cfb9d831c3aaae52dd89269
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-01-02 16:45:18 +0200

    Add let and const

---
+++ index.js
@@ -0,0 +1,42 @@
+// alert(123)
+
+  // console.log(123);
+  // console.log(12.3);
+  // console.log("text");
+  // console.log(true);
+  // console.log("true");
+  // console.log(null);
+  // console.log(undefined)
+
+/* починати з букви, з $ або  _ 
+ !!!не використовувати пробіл 
+ регістрозалежне
+ відповідає на питання, щ в ній 
+ */
+
+ let userName = "Brad";
+ console.log(userName);
+
+ let userAge;   // оголошення
+ user = 59;     // ініціфлізація
+
+ console.log(userName);
+ console.log(userAge);
+
+ const userYearOfBirth = "";
+
+//  userAge(60);
+ userName = "Bradly";
+
+ const userGender="male";
+//  userGender="female"; error;
+
+ const AMOUNT_DAYS_WEEK = 7;
+
+ let number = 12;
+ {
+  let number =66;
+  console.log(number);
+ }
+
+ console.log(number)
